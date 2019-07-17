3fe35079f06719c0c3e1c4bfbe4b36d57010eb94
colinm <colinm@agylia.com>
2019-07-17 12:28:28 +0100

    Initial project load

---
+++ stretchy-headers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stretchy-headers
+//
+//  Created by Colin Milhench on 17/07/2019.
+//  Copyright © 2019 Colin Milhench. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
